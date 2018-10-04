ef0bd37b1615f91c6e69c856e0302eeca1de51ad
Giovanni De Toni <giovanni.det@gmail.com>
2018-10-04 15:18:53 +0200

    Remove unused import.

--- ds2lab/src/main/java/com/projects/detoni_zampieri/message/NodeListMessage.java
+++ ds2lab/src/main/java/com/projects/detoni_zampieri/message/NodeListMessage.java
@@ -1,8 +1,6 @@
 package com.projects.detoni_zampieri.message;
 
 import akka.actor.ActorRef;
-import com.projects.detoni_zampieri.message.Message;
-
 import java.util.ArrayList;
 
 public class NodeListMessage extends Message {
