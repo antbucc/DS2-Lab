1ebaeb4264b2a1ebd09bc80eaafa91938c8025fc
andreazampieri <andreazampieri95@gmail.com>
2018-10-04 18:34:18 +0200

    first commit for PushPullActor

--- ds2lab/src/main/java/com/projects/detoni_zampieri/lab2/actor/PushPullActor.java
+++ ds2lab/src/main/java/com/projects/detoni_zampieri/lab2/actor/PushPullActor.java
@@ -1,4 +1,41 @@
 package com.projects.detoni_zampieri.lab2.actor;
 
+import com.projects.detoni_zampieri.lab2.message.PullReplyMessage;
+import com.projects.detoni_zampieri.lab2.message.PushPullMessage;
+import com.projects.detoni_zampieri.lab2.message.TimeoutMessage;
+
 public class PushPullActor extends Actor {
+	
+	@Override
+	public void onReceive(Object message) {
+		if(message instanceof PushPullMessage) {
+			onPushPullMessage((PushPullMessage)message);
+		}
+		else if(message instanceof PullReplyMessage) {
+			onPullReplyMessage((PullReplyMessage)message);
+		}
+		else if(message instanceof TimeoutMessage) {
+			onTimeoutMessage((TimeoutMessage)message);
+		}
+		else unhandled(message);
+	}
+	
+	public void onPushPullMessage(PushPullMessage message) {
+		if(this.value.getTimestamp().before(message.value.getTimestamp())) {
+			this.value = message.value;
+		}
+		else if(this.value.getTimestamp().after(message.value.getTimestamp())) {
+			PullReplyMessage rep = new PullReplyMessage(this.value);
+			getSender().tell(rep, getSelf());
+		}
+	}
+	
+	public void onPullReplyMessage(PullReplyMessage message) {
+		
+	}
+	
+	public void onTimeoutMessage(TimeoutMessage message) {
+		PushPullMessage msg = new PushPullMessage(this.value);
+		this.sendMessage(msg);
+	}
 }
--- ds2lab/src/main/java/com/projects/detoni_zampieri/lab2/message/PushPullMessage.java
+++ ds2lab/src/main/java/com/projects/detoni_zampieri/lab2/message/PushPullMessage.java
@@ -2,7 +2,7 @@ package com.projects.detoni_zampieri.lab2.message;
 
 
 public class PushPullMessage extends Message{
-	private EpidemicValue value;
+	public EpidemicValue value;
 	
 	public PushPullMessage(EpidemicValue value) {
 		this.value = value;
